15c54aa717ff2ae027cce8a34d5af61a12fc97b6
anderson-neo <atnak.inar@gmail.com>
2016-11-24 21:20:24 +0530

    chat activity UI and fixes

--- app/src/main/java/in/splitc/share/views/ChatActivity.java
+++ app/src/main/java/in/splitc/share/views/ChatActivity.java
@@ -39,7 +39,7 @@ public class ChatActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.accept_ride);
+        setContentView(R.layout.activity_chat);
 
         prefs = getSharedPreferences("application_settings", 0);
         zapp = (ZApplication) getApplication();
