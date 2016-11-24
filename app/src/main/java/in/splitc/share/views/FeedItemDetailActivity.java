f8c6f0b2b4ec5b84b38d86ed34469d13687ecd26
anderson-neo <atnak.inar@gmail.com>
2016-11-24 20:06:08 +0530

    back arrow fixes, new ride fixes

--- app/src/main/java/in/splitc/share/views/FeedItemDetailActivity.java
+++ app/src/main/java/in/splitc/share/views/FeedItemDetailActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -11,18 +12,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import in.splitc.share.R;
 import in.splitc.share.ZApplication;
 import in.splitc.share.adapters.GooglePlaceAutocompleteAdapter;
 import in.splitc.share.data.Address;
-import in.splitc.share.data.Ride;
+import in.splitc.share.data.Feed;
 import in.splitc.share.utils.CommonLib;
-import in.splitc.share.utils.UploadManager;
-import in.splitc.share.utils.UploadManagerCallback;
-import okhttp3.FormBody;
+import in.splitc.share.utils.ImageLoader;
 
 /**
  * Created by neo on 06/11/16.
@@ -37,9 +36,10 @@ public class FeedItemDetailActivity extends AppCompatActivity  {
     private LayoutInflater inflater;
     private ProgressDialog zProgressDialog;
 
-    private Ride ride;
+    private Feed ride;
     private Address startLocationObject;
     private AutoCompleteTextView startLocation, dropLocation;
+    private ImageLoader loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,16 +52,14 @@ public class FeedItemDetailActivity extends AppCompatActivity  {
         width = getWindowManager().getDefaultDisplay().getWidth();
         height = getWindowManager().getDefaultDisplay().getHeight();
         inflater = LayoutInflater.from(mContext);
+        loader = new ImageLoader(this, zapp);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(getResources().getString(R.string.confirm_booking));
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if (getIntent() != null && getIntent().hasExtra("feedItem")) {
+            ride = (Feed) getIntent().getSerializableExtra("feedItem");
+        } else
+            finish();
 
-        if (getIntent() != null) {
-            if (getIntent().hasExtra("ride"))
-                ride = (Ride) getIntent().getSerializableExtra("ride");
-        }
+        setUpActionBar();
 
         View yourLocationContainer = findViewById(R.id.your_location_container);
 
@@ -83,7 +81,36 @@ public class FeedItemDetailActivity extends AppCompatActivity  {
             }
         });
 
+        View userLocationContainer = findViewById(R.id.location_container);
+        ((TextView)userLocationContainer.findViewById(R.id.start_location)).setText(ride.getFromAddress());
+        ((TextView)userLocationContainer.findViewById(R.id.drop_location)).setText(ride.getToAddress());
+        ((TextView)findViewById(R.id.pickup_timer)).setText(CommonLib.getTimeFormattedString(ride.getCreated()));
+        if (ride.getDescription() != null && ride.getDescription().length() > 0)
+            ((TextView)findViewById(R.id.description)).setText(ride.getDescription());
+        else
+            findViewById(R.id.description).setVisibility(View.GONE);
+
+        if (ride.getFeedType() == CommonLib.FEED_TYPE_RIDE) {
+            ((TextView)findViewById(R.id.user_trip_title)).setText(getResources().getString(R.string.travel_title_string, ride.getUser().getUserName()));
+        } else
+            ((TextView)findViewById(R.id.user_trip_title)).setText(getResources().getString(R.string.need_ride_string, ride.getUser().getUserName()));
+
+        if (ride.getUser().getProfilePic() != null)
+            loader.setImageFromUrlOrDisk(ride.getUser().getProfilePic(), (ImageView) findViewById(R.id.user_image), "", width, height, false);
+        else
+            ((ImageView)findViewById(R.id.user_image)).setImageBitmap(CommonLib.getBitmap(this, R.drawable.user, width, height));
+
+
         setListeners();
+    }
+
+    private void setUpActionBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(getResources().getString(R.string.confirm_booking));
     }
 
     private void setListeners() {
